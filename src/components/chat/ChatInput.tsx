09c53d9997d88054f82ae05c86684d9ee8e15416
unknown <vietnemo123@gmail.com>
2023-10-19 22:49:41 +1100

    response lmao

--- src/components/chat/ChatInput.tsx
+++ src/components/chat/ChatInput.tsx
@@ -1,14 +1,17 @@
-import React from "react";
 import { Send } from "lucide-react";
 import { Button } from "../ui/button";
 import { Textarea } from "../ui/textarea";
 import { useContext, useRef } from "react";
+import { ChatContext } from "./ChatContext";
 
-type ChatInputProps = {
+interface ChatInputProps {
   isDisabled?: boolean;
-};
+}
 
-const ChatInput = (isDisabled: ChatInputProps) => {
+const ChatInput = ({ isDisabled }: ChatInputProps) => {
+  const { addMessage, handleInputChange, isLoading, message } =
+    useContext(ChatContext);
+
   const textareaRef = useRef<HTMLTextAreaElement>(null);
 
   return (
@@ -22,29 +25,30 @@ const ChatInput = (isDisabled: ChatInputProps) => {
                 ref={textareaRef}
                 maxRows={4}
                 autoFocus
-                // onChange={handleInputChange}
-                // value={message}
-                // onKeyDown={(e) => {
-                //   if (e.key === "Enter" && !e.shiftKey) {
-                //     e.preventDefault();
+                onChange={handleInputChange}
+                value={message}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter" && !e.shiftKey) {
+                    e.preventDefault();
 
-                //     addMessage();
+                    addMessage();
 
-                //     textareaRef.current?.focus();
-                //   }
-                // }}
+                    textareaRef.current?.focus();
+                  }
+                }}
                 placeholder="Enter your question..."
                 className="resize-none pr-12 text-base py-3 scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch"
               />
+
               <Button
-                // disabled={isLoading || isDisabled}
+                disabled={isLoading || isDisabled}
                 className="absolute bottom-1.5 right-[8px]"
                 aria-label="send message"
-                // onClick={() => {
-                //   addMessage();
+                onClick={() => {
+                  addMessage();
 
-                //   textareaRef.current?.focus();
-                // }}
+                  textareaRef.current?.focus();
+                }}
               >
                 <Send className="h-4 w-4" />
               </Button>
