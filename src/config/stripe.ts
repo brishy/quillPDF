7946d73d99de47b36282d1b50579e0972de034e0
unknown <vietnemo123@gmail.com>
2023-10-21 12:52:43 +1100

    stripe

---
+++ src/config/stripe.ts
@@ -0,0 +1,28 @@
+export const PLANS = [
+  {
+    name: "Free",
+    slug: "free",
+    quota: 10,
+    pagesPerPdf: 10,
+    price: {
+      amount: 0,
+      priceIds: {
+        test: "",
+        production: "",
+      },
+    },
+  },
+  {
+    name: "Pro",
+    slug: "pro",
+    quota: 100,
+    pagesPerPdf: 50,
+    price: {
+      amount: 20,
+      priceIds: {
+        test: "price_1O3U3GLBfKe4mW7KeZUOEcFJ",
+        production: "",
+      },
+    },
+  },
+];
